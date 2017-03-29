d49cc96bb8b9a422b735c5f96a81cd7b76d577e2
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-03-29 16:47:12 -0500

    builds out function structure for generate Team

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -16,24 +16,44 @@ function init(){
 function enable(){
   console.log( "inside enable" );
   //on click generateTeam -  gets PEOPLE
+  generateTeam();
   //on click save team - posts Team
+  saveTeam();
   //on click toggle between views - gets teams
+  generateAllTeams();
 }
 
 
 //event handlers
-
   //click generate teams
+  function generateTeam(){
+    console.log( "inside generate team" );
+    retrieveNumTeams();
+    getPeople();
+  }//ends generateTeam
 
   //click save teams
+  function saveTeam(){
+      console.log( "inside saveTeam" );
+  }//ends saveTeam
 
   //toggle past teams
+
+//logic
+function retrieveNumTeams(){
+  console.log( "inside retrieveNumTeams" );
+
+}//ends retrieveNumTeams
 
 //DOM methods
 
 //REST interface
 
   //get /people -GET PEOPLE
+  function getPeople(){
+    console.log( "inside getPeople" );
+
+  }//ends ajax get getPeople
 
   //get /teams -GET PAST teams
 
