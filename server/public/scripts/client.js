1907fac3c41903db16b4a436e0807fcdc3f88818
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-03-29 16:26:33 -0500

    adds basic file structure to client.js

---
+++ server/public/scripts/client.js
@@ -0,0 +1,25 @@
+//global variables
+
+//document ready
+
+//function init
+
+//function enable
+
+//event handlers
+
+  //click generate teams
+
+  //click save teams
+
+  //toggle past teams
+
+//DOM methods
+
+//REST interface
+
+  //get /people -GET PEOPLE
+
+  //get /teams -GET PAST teams
+
+  //post /teams -POST CURRENT teams
